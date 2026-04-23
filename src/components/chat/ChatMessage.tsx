2a0850a1fa626d37097333f1d65c37901146c3cc
brutal-45 <creatorsports81@gmail.com>
2026-04-23 11:19:11 +0530

    Update ChatMessage.tsx

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -10,7 +10,7 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark, oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { jsPDF } from 'jspdf';
+import { jsPDF } from 'jspdf'; 
 
 interface ChatMessageProps {
   message: Message;

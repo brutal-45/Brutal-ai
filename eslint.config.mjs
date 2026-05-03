f2bcf2fe43e637767713fd73e65c365417659190
brutal-45 <creatorsports81@gmail.com>
2026-05-03 15:36:50 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, { 
   rules: {
     // TypeScript rules
     "@typescript-eslint/no-explicit-any": "off",

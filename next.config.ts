ca4198cb63797963e23d287afaf489ba7205557e
brutal-45 <creatorsports81@gmail.com>
2026-05-03 15:36:56 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
   // React configuration
   reactStrictMode: false,
   
-  // Image optimization
+  // Image optimization 
   images: {
     remotePatterns: [
       {

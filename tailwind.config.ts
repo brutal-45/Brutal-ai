86accbf0ccb39d2b8d2f5db38cbe5c9cfeaf4ca7
brutal-45 <creatorsports81@gmail.com>
2026-06-17 15:11:36 +0530

    Fix indentation in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ], 
-  theme: {  
+  theme: {
   	extend: {
   		colors: {
   			background: 'hsl(var(--background))',
